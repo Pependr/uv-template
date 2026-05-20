97261b2ac14512cf1e4efd5a5a67e3d8bb54f850
Pependr <antonkudrikov88@gmail.com>
2026-05-20 22:07:29 +0300

    Update init script

--- init.sh
+++ init.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-uv init $1
-git init
+uv init $1 --no-pin-python --vcs git --package
+
+mkdir -pv tests/$(basename $PWD)
+touch tests/$(basename $PWD)/test_$(basename $PWD).py
+printf 'def test_my_project() -> None:\n\tassert "I <3 python"\n' >> tests/$(basename $PWD)/test_$(basename $PWD).py
 
 uv venv
 source .venv/bin/activate
