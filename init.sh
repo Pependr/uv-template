9c2e9735cf6cc9e5a886bdf2b2d6a0d1eda8e4cb
Pependr <antonkudrikov88@gmail.com>
2026-06-24 11:35:36 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,16 +2,15 @@
 
 uv init $1 --no-pin-python --vcs git --package
 
-mkdir -pv tests/$(basename $PWD)
-touch tests/$(basename $PWD)/test_$(basename $PWD).py
-printf 'def test_my_project() -> None:\n\tassert "I <3 python"\n' >> tests/$(basename $PWD)/test_$(basename $PWD).py
+mkdir -pv $"tests/{$PWD}/"
+printf 'def test_my_project() -> None:\n    assert "I <3 python"\n' >> $"tests/{$PWD}/test_{$PWD}.py"
+
+uv python install $2
 
 uv venv
 source .venv/bin/activate
 
-uv python install $2
-
-uv add ruff pytest pytest-cov pytest-mock --dev
+uv add ruff pytest pytest-cov pytest-mock honk-parser --dev
 uv sync --dev
 
 unlink init.sh
